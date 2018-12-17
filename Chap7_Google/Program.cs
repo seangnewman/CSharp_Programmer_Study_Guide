581e4b8b3a59c56b3f388e8796bc6a20235549f2
Sean Newman <seangnewman@msn.com>
2018-12-17 13:48:06 -0700

    Completed chapter 7 & 8 challenges

---
+++ Chap7_Google/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap7_Google
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> htmlString = ReturnHTML;
+
+            string name = "https://www.google.com";
+
+            Console.WriteLine(htmlString(name));
+
+        }
+
+        public  static string ReturnHTML( string url)
+        {
+            if (!string.IsNullOrWhiteSpace(url))
+            {
+                using (HttpClient client = new HttpClient())
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                    using(HttpContent content = response.Content)
+                {
+                    return content.ReadAsStringAsync().Result;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Chapter8_URL/HTMLUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+namespace Chapter8_URL
+{
+    public static class HTMLUtilities
+    {
+        public static string ReturnHTML(string url)
+        {
+            if (!string.IsNullOrWhiteSpace(url))
+            {
+                using (HttpClient client = new HttpClient())
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                using (HttpContent content = response.Content)
+                {
+                    return content.ReadAsStringAsync().Result;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
