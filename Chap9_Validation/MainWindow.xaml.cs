d570f99548b0aa3476b81723289dcc6e4c93403a
Sean Newman <seangnewman@msn.com>
2018-12-18 20:42:41 -0700

    Completed challenge

---
+++ Chap9_Validation/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Chap9_Validation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool allFieldsPopulated()
+        {
+            return ((txtEmail.Text.Length > 0) &&
+                  (txtPhone.Text.Length > 0) &&
+                  (txtDOB.Text.Length > 0) &&
+                  (txtZip.Text.Length > 0) &&
+                  (txtWebsite.Text.Length > 0)) ;
+        }
+
+        public void resetForm( TextBox txtBox)
+        {
+            txtBox.Text = string.Empty;
+            frmButton.IsEnabled = false;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            frmButton.IsEnabled = false;
+        }
+
+        private void frmButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+            // Your application must validate their information by using regular expression and must handle
+            // exception in case the user enters invalid values.
+
+            // Validate email information
+            if(!TextValidation.TestEmail.IsEmail(txtEmail.Text))
+            {
+                resetForm(txtEmail);
+                return;
+            }
+
+            if (!TextValidation.TestPhoneNumber.IsUsPhoneNumber(txtPhone.Text))
+            {
+                resetForm(txtPhone);
+                return;
+            }
+
+            if (!DateTime.TryParse(txtDOB.Text, out var dt))
+            {
+                resetForm(txtDOB);
+                return;
+            }
+
+            if (!TextValidation.TestZipCode.IsZipCode(txtZip.Text))
+            {
+                resetForm(txtZip);
+                return;
+            }
+
+            if(!Uri.IsWellFormedUriString(txtWebsite.Text, UriKind.RelativeOrAbsolute))
+            {
+                resetForm(txtWebsite);
+                return;
+            }
+
+            MessageBox.Show("Success");
+
+        }
+
+        private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (allFieldsPopulated())
+                frmButton.IsEnabled = true;
+        }
+
+        private void txtPhone_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (allFieldsPopulated())
+                frmButton.IsEnabled = true;
+        }
+
+        private void txtDOB_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (allFieldsPopulated())
+                frmButton.IsEnabled = true;
+        }
+
+        private void txtZip_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (allFieldsPopulated())
+                frmButton.IsEnabled = true;
+        }
+
+        private void txtWebsite_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (allFieldsPopulated())
+                frmButton.IsEnabled = true;
+        }
+    }
+}
---
+++ Chap9_Validation/TextValidation.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Chap9_Validation
+{
+    class TextValidation
+    {
+
+        public static class TestEmail
+        {
+            /// <summary>
+            /// Regular expression, which is used to validate an E-Mail address.
+            /// </summary>
+            public const string MatchEmailPattern =
+                      @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+               + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+               + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+               + @"([a-zA-Z0-9]+[\w-]+\.)+[a-zA-Z]{1}[a-zA-Z0-9-]{1,23})$";
+
+           
+            /// <summary>
+            /// Checks whether the given Email-Parameter is a valid E-Mail address.
+            /// </summary>
+            /// <param name="email">Parameter-string that contains an E-Mail address.</param>
+            /// <returns>True, when Parameter-string is not null and 
+            /// contains a valid E-Mail address;
+            /// otherwise false.</returns>
+            public static bool IsEmail(string email)
+            {
+                if (email != null)
+                    return Regex.IsMatch(email, MatchEmailPattern);
+                else return false;
+            }
+
+           
+        }
+
+        public static class TestPhoneNumber
+        {
+            public const string MatchUSPhonePattern =
+               @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
+
+            public static bool IsUsPhoneNumber(string phoneNumber)
+            {
+                if (phoneNumber != null)
+                    return Regex.IsMatch(phoneNumber, MatchUSPhonePattern);
+                else
+                    return false;
+            }
+        }
+
+        public static class TestZipCode
+        {
+            public const string MatchZipCodePattern =
+                @"^[0-9]{5}(?:-[0-9]{4})?$";
+
+            public static bool IsZipCode(string zipCode)
+            {
+                  if (zipCode != null)
+                    return Regex.IsMatch(zipCode, MatchZipCodePattern);
+                else
+                    return false;
+            }
+        }
+
+    }
+}
